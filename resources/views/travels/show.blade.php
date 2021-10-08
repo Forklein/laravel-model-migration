801a239102a7e0ebc3f74fcdc1d315b607bf12ae
Forklein <giuseppepisani2@gmail.com>
2021-10-08 18:55:22 +0200

    Add show blade

---
+++ resources/views/travels/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('title', $travel->destination)
+
+@section('content')
+
+<section id="travel">
+    <div class="container">
+        <div class="card border-info mb-3 col-6 mx-auto text-center">
+            <div class="card-header bg-transparent border-success">
+                Destinazione: {{$travel->destination}}
+            </div>
+            <div class="card-body text-success">
+              <h5 class="card-title">Mezzo di trasporto: {{$travel->transport}}</h5>
+              <p class="card-text">Descrizione: {{$travel->description}}</p>
+            </div>
+            <div class="card-footer bg-transparent border-success">Prezzo: {{$travel->price}}€</div>
+        </div>
+    </div>
+</section>
+
+@endsection
