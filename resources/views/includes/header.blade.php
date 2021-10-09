9606223ab8b895a2cc576cb8358981910ee738d5
Forklein <giuseppepisani2@gmail.com>
2021-10-09 19:35:26 +0200

    add class active dynamic for home and travels.*

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,10 @@
 <header>
-    <ul class="nav nav-pills bg-dark">
+    <ul class="nav nav-pills bg-dark p-3">
         <li class="nav-item">
-          <a class="nav-link text-info" aria-current="page" href="{{ route('home') }}">Home</a>
+          <a class="nav-link text-white {{ request()->routeIs('home') ? 'active' : ''}}" aria-current="page" href="{{ route('home') }}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-info" href="{{ route('travels.index') }}">Travels</a>
+          <a class="nav-link text-white {{ request()->routeIs('travels.*') ? 'active' : ''}}" href="{{ route('travels.index') }}">Travels</a>
         </li>
       </ul>
 </header>
